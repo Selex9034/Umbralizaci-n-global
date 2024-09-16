469df1086693a744a42e51e6f81bda2da777b4d6
ay1309 <161781468+ay1309@users.noreply.github.com>
2024-09-16 14:10:28 -0600

    ya

--- umbralizacion.py
+++ umbralizacion.py
@@ -7,24 +7,65 @@ def escala_grises(array):
     gray_img = gray_values.astype(np.uint8)
     return gray_img
 
+def umbralizacion_global(img_gris, valorT_list):
+    resultados = {}
+    
+    # se calcula T0 con la media
+    T0 = np.mean(img_gris)
+    
+    for valorT in valorT_list:
+        T = T0
+        dif = np.inf
+        
+        while dif > valorT:  # división de píxeles de acuerdo al umbral
+            grupo1 = img_gris[img_gris <= T]
+            grupo2 = img_gris[img_gris > T]
+            
+            mean1 = np.mean(grupo1) if len(grupo1) > 0 else 0
+            mean2 = np.mean(grupo2) if len(grupo2) > 0 else 0
+            
+            T_new = (mean1 + mean2) / 2  # umbral nuevo con promedio de los dos grupos
+            
+            dif = abs(T_new - T)
+            T = T_new
+        
+        umbralizada = np.where(img_gris > T, 255, 0).astype(np.uint8)
+        resultados[valorT] = umbralizada
+    
+    return resultados
 
-#leer imagen
-img=np.array(Image.open('leopardo.jpg'))
+imagenes = [
+    'C:/Users/Anna Beristain/Downloads/practicaV/Umbralizaci-n-global/leopardo.jpg', 
+    'C:/Users/Anna Beristain/Downloads/practicaV/Umbralizaci-n-global/flor.jpg', 
+    'C:/Users/Anna Beristain/Downloads/practicaV/Umbralizaci-n-global/perro.jpg'
+]
 
-img_gris = escala_grises(img)
+imgs_grises = [escala_grises(np.array(Image.open(img))) for img in imagenes]
 
-plt.figure(figsize=(10, 5))
-plt.subplot(1, 2, 1)
-plt.imshow(img_gris, cmap='gray')
-plt.title('Imagen en escala de grises')
-plt.axis('off')
+valorT_list = [0, 0.05, 0.1]
 
+for idx, img_gris in enumerate(imgs_grises):
+    umbralizadas = umbralizacion_global(img_gris, valorT_list)
+    
+    plt.figure(figsize=(15, 10))
 
-plt.subplot(1, 2, 2)
-plt.hist(img_gris.ravel(), 256, [0, 256])
-plt.title('Histograma')
-plt.xlabel('Intensidad de píxel')
-plt.ylabel('Frecuencia')
+    plt.subplot(2, 3, 1)  
+    plt.imshow(img_gris, cmap='gray')
+    plt.title(f'Imagen en escala de grises {idx+1}')
+    plt.axis('off')
 
-plt.tight_layout()
-plt.show()
+    plt.subplot(2, 3, 2)
+    plt.hist(img_gris.ravel(), 256, [0, 256])
+    plt.title(f'Histograma {idx+1}')
+    plt.xlabel('Intensidad de píxel')
+    plt.ylabel('Frecuencia')
+
+    for i, valorT in enumerate(valorT_list, start=1):
+        plt.subplot(2, 3, i + 3) 
+        plt.imshow(umbralizadas[valorT], cmap='gray')
+        plt.title(f'Umbralización {idx+1} (ΔT = {valorT})')
+        plt.axis('off')
+
+    plt.tight_layout()
+    plt.show()
+
